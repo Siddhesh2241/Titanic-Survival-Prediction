889ced1270f8f110622d50282478f4ae82051df0
Siddhesh <siddhesh.nikam22@gmail.com>
2024-11-05 15:11:56 +0530

    Predict data Pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,60 @@
+import sys
+import pandas as pd 
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def Predict(self,features):
+        try:
+            model_path = "artifacts\model.pkl"
+            preprocessor_path =  'artifacts\Preprocessor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    
+    class CustomData:
+        def __init__(self,
+                    PassengerId:int,
+                    Pclass:int,
+                    Age:int,
+                    SibSp:int,
+                    Parch:int,
+                    Fare:int,
+                    Sex:str,
+                    Embarked:str  ):
+        
+                    self.PassengerId = PassengerId
+                    self.Pclass = Pclass
+                    self.Age = Age
+                    self.SibSp = SibSp
+                    self.Parch = Parch
+                    self.Fare = Fare
+                    self.Sex = Sex
+                    self.Embarked = Embarked
+
+        def get_data_as_data_frame(self):
+             try:
+                  custom_data_input_dict = {
+                       "PassengerId" : [self.PassengerId],
+                       "Pclass": [self.Pclass],
+                       "Age":[self.Age],
+                       "SibSp":[self.SibSp],
+                       "Parch":[self.Parch],
+                       "Fare":[self.Fare],
+                       "Sex": [self.Sex],
+                       "Embarked": [self.Embarked]
+                  }
+
+             except Exception as e:
+                  raise CustomException(e,sys)
