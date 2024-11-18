705242d9a164045028f9c447372595e340b190de
Siddhesh <siddhesh.nikam22@gmail.com>
2024-11-18 14:40:10 +0530

    Change path

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd 
 from src.exception import CustomException
@@ -5,14 +6,15 @@ from src.utils import load_object
 
 class PredictPipeline:
     def __init__(self):
-        pass
+        self.model_path = os.path.join("artifacts","model.pkl")
+        self.preprocessor_path = os.path.join("artifacts","preprocessor.pkl")
 
     def predict(self,features):
         try:
-            model_path = "artifacts\model.pkl"
-            preprocessor_path = 'artifacts\preprocessor.pkl'
-            model = load_object(file_path=model_path)
-            preprocessor = load_object(file_path=preprocessor_path)
+            #model_path = "artifacts\model.pkl"
+            #preprocessor_path = 'artifacts\preprocessor.pkl'
+            model = load_object(file_path=self.model_path)
+            preprocessor = load_object(file_path=self.preprocessor_path)
 
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
